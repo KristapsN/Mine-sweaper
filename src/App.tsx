bce8c932113be2001c23f2573a44b31f9eed2a21
KristapsN <kristaps.nogobods@gmail.com>
2020-12-14 18:04:59 +0200

    Open multiple empty fielsd

--- src/App.tsx
+++ src/App.tsx
@@ -14,28 +14,78 @@ const App = () => {
     example
   ]);
   const [mesage, setMesage] = useState('');
+  const [bombCount, setBombCount] = useState(0);
+
+  const checkWin = () => {
+    const winLength = fieldSquers.length - bombPlaces.length;
+    console.log('Test win');
+    const winField = fieldSquers.filter(item => item.open === true);
+    console.log('Now', winField.length, 'Should', winLength - 1);
+    if (winField.length === winLength) {
+      setMesage('You Win!');
+    }
+  };
+  const checkIfZero = (x: number, y: number) => {
+    const chekZeros = [...fieldSquers];
+    chekZeros.map((item) => {
+      for (let i = -1; i < 2; i++) {
+        for (let l = -1; l < 2; l++) {
+          if (item.x === x + 10 * i && item.y === y + 10 * l
+            && item.bomb === false) {
+            item.open = true;
+          }
+        }
+      }
+    });
+  };
+
+  const checkNextOpenZeroStep = () => {
+    const chekNextZeros2 = [...fieldSquers];
+    const chekNextZeros = fieldSquers.filter(item => item.bombNumber === 0 && item.open === true);
+    fieldSquers.map((item2) => {
+      chekNextZeros.map((item3) => {
+        for (let i = -1; i < 2; i++) {
+          for (let l = -1; l < 2; l++) {
+            if (item2.x === item3.x + 10 * i && item2.y === item3.y + 10 * l && item2.bomb === false) {
+              item2.open = true;
+              setFieldSquers(chekNextZeros2);
+            }
+          }
+        }
+      });
+    });
+  };
+  const callCheckNext = () => {
+    for (let i = 0; i < 10; i++) {
+      checkNextOpenZeroStep();
+    }
+  };
   const openHandler = (x: number, y: number) => {
     const openFieldSquers = [...fieldSquers];
     openFieldSquers.map((item) => {
       if (item.x === x && item.y === y) {
+
         item.open = true;
-        if (item.bomb ){
-          setMesage('You Loese');
+        if (item.bomb) {
+          youLose();
         }
-      } 
-      if (item.bombNumber === 0) {
-        console.log('true');
-        for (let i = -1; i < 2; i++) {
-          for (let l = -1; l < 2; l++) {
-            if (item.x === x + 10 * i && item.y === y + 10 * l
-              && item.bomb === false) {
-              item.open = true;
-            }
-          } 
+        if (item.bombNumber === 0) {
+          checkIfZero(x, y);
         }
       }
     });
     setFieldSquers(openFieldSquers);
+    callCheckNext();
+    checkWin();
+  };
+
+  const youLose = () => {
+    setMesage('You Loese');
+    const loseOpenAll = [...fieldSquers];
+    loseOpenAll.map((item) => {
+      item.open = true;
+      setFieldSquers(loseOpenAll);
+    });
   };
 
   const draw = () => {
@@ -49,7 +99,7 @@ const App = () => {
           firstCircel: false, secondCircle: false, bombNumber: 0
         };
         newFieldSquers.push(generate);
-        setFieldSquers(newFieldSquers);      
+        setFieldSquers(newFieldSquers);
       }
     }
   };
@@ -57,15 +107,24 @@ const App = () => {
   const putBombs = () => {
     const bombField = [...fieldSquers];
     bombField.map((item) => {
-      if (item.x / 10 %2 === 0 && item.y / 10 % 2 === 0) {
-        bombPlaces.push([item.x, item.y]);
-        item.bomb = true;
+      for (let i = 0; i < 11; i++) {
+        const num = Math.floor(Math.random() * 11);
+        const num2 = Math.floor(Math.random() * 11);
+        if (item.x / 10 === num && item.y / 10 === num2) {
+          bombPlaces.push([item.x, item.y]);
+          item.bomb = true;
+        }
       }
     });
     setFieldSquers(bombField);
+    bombCounter();
   };
 
-  // console.log(bombPlaces);
+  const bombCounter = () => {
+    const bombLength = fieldSquers.filter(item => item.bomb === true);
+    setBombCount(bombLength.length);
+  };
+
   const putBombNumbers = () => {
     const bombNumberField = [...fieldSquers];
     fieldSquers.map((item) => {
@@ -98,7 +157,9 @@ const App = () => {
         <div className="row">
           <div className="col-xs-offset-3 col-xs-6">
             <button type="button" onClick={() => start()}>Start</button>
+            <button type="button" onClick={() => callCheckNext()}>Test</button>
             <h2>{mesage}</h2>
+            <h2>{bombCount}</h2>
             <div className="game--grid">
               {fieldSquers.map((item) =>
                 <div key={item.id}>
